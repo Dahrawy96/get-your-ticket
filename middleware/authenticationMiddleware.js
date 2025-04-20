ec1bed93bb08e3898cbd2fc224487072642d5e74
Lujyne Selim <lujyneselim@gmail.com>
2025-04-20 16:53:03 +0200

    hatet el event routes and the 2 middlewares

--- middleware/authenticationMiddleware.js
+++ middleware/authenticationMiddleware.js
@@ -1,22 +1,39 @@
+//// Bring in the jsonwebtoken package so we can verify tokens
 const jwt = require("jsonwebtoken");
+
+//get the secret key to decode and verify the jwt token
 const secretKey = process.env.JWT_SECRET;
 
-// 🍪 Middleware to verify token from cookies
+
+//haamel function middleware to protect el routes 
+//bacheck law el user logged in aw la
+// law 3ando token aw la
 module.exports = function authenticationMiddleware(req, res, next) {
-  const cookie = req.cookies;
 
-  if (!cookie || !cookie.token) {
-    return res.status(401).json({ message: "No token provided in cookies" });
+   // look for the token in the request headers specifically fe "Authorization"
+  const authHeader = req.headers.authorization;
+
+
+
+//law mafeesh token aw el token mesh sah doesnt start with "Bearer "
+// return 401 Unauthorized
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided in Authorization header" });
   }
+   // basplit el token mn el bearer(string) 3ashan a3raf el token bas
+  const token = authHeader.split(" ")[1];
 
-  const token = cookie.token;
+  try {
+     // try to verify (decode) the token using the secret key
+    const decoded = jwt.verify(token, secretKey);
+    // if successful attach the decoded user information to the request object
+    // so that it can be accessed in the next middleware or route handler
+    req.user = decoded.user;
 
-  jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ message: "Invalid token" });
-    }
-
-    req.user = decoded.user; // same structure: { id, role }
+    // move to the next step (controller or next middleware)
     next();
-  });
+  } catch (err) {
+    //law token expired aw fake error appears
+    return res.status(403).json({ message: "Invalid token" });
+  }
 };
