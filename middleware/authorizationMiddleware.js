6cf23844119c48d9d25360129e2d1149eda6db4c
Lujyne Selim <lujyneselim@gmail.com>
2025-04-20 17:17:08 +0200

    newww authorizatiuon middlewareee

--- middleware/authorizationMiddleware.js
+++ middleware/authorizationMiddleware.js
@@ -1,10 +1,18 @@
+// export- function that takes in allowed roles zay el admin w el organizer)
 module.exports = function authorizationMiddleware(roles) {
-    return (req, res, next) => {
-      const userRole = req.user.role;
-      if (!roles.includes(userRole)) {
-        return res.status(403).json({ message: "Unauthorized access" });
-      }
-      next();
-    };
+
+  //returns another function (middleware) that handles the request
+  return (req, res, next) => {
+    // Get the role of the current user from the token el howa already verified in authentication middleware
+    const userRole = req.user.role;
+
+    // checks if user is actually allowed fel user roles
+    if (!roles.includes(userRole)) {
+      // If not allowed, return a 403 Forbidden error
+      return res.status(403).json({ message: "Unauthorized access" });
+    }
+
+    // law allowed continue to the next middleware or route handler
+    next();
   };
-  
+};
